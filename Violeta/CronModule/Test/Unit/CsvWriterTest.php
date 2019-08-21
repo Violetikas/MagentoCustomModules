8e0816c8c70b9469cf20838030da7a49c7beca0d
violeta <violetikas@yahoo.com>
2019-08-21 13:24:11 +0300

    Refactoring

--- Violeta/CronModule/Test/Unit/CsvWriterTest.php
+++ Violeta/CronModule/Test/Unit/CsvWriterTest.php
@@ -27,8 +27,35 @@ class CsvWriterTest extends TestCase
     public function testWrite()
     {
         $writer = new CsvWriter();
-        $customer = json_decode('{"entity_id":"11", "website_id":"1", "email":"whatever1@gmail.com", "group_id":"1", "increment_id":null, "store_id":"2", "created_at":"2019-08-18 13:18:47", "updated_at":"2019-08-18 13:18:48", "is_active":"1", "disable_auto_group_change":"0", "created_in":"Second store view", "prefix":null, "firstname":"Petras", "middlename":null, "lastname":"Pirmasis", "suffix":null, "dob":null, "password_hash":"2d4788574f6b918a4d1fd9435f25621b0cae38559e85fdeb45856b3c9df7d6cb:kg2Jzguul7XIeXDI085fNLTK9B1MvqTB:1", "rp_token":"mz5qlwATAuQ7id3LD5JXAaDOnwpB1ZGO", "rp_token_created_at":"2019-08-18 13:18:48", "default_billing":null, "default_shipping":null, "taxvat":null, "confirmation":null, "gender":null, "failures_num":"0", "first_failure":null, "lock_expires":null}',
-            true);
+        $customer = json_decode(
+            '{"entity_id":"11",
+             "website_id":"1",
+              "email":"whatever1@gmail.com",
+               "group_id":"1",
+                "increment_id":null,
+                 "store_id":"2",
+                  "created_at":"2019-08-18 13:18:47",
+                   "updated_at":"2019-08-18 13:18:48",
+                    "is_active":"1",
+                     "disable_auto_group_change":"0",
+                      "created_in":"Second store view",
+                       "prefix":null, "firstname":"Petras",
+                        "middlename":null,
+                         "lastname":"Pirmasis",
+                          "suffix":null, "dob":null,
+                           "password_hash":"2d4788574f6b918a4d1fd9435f25621b0cae38559e85fdeb45856b3c9df7d6cb:kg2Jzguul7XIeXDI085fNLTK9B1MvqTB:1",
+                            "rp_token":"mz5qlwATAuQ7id3LD5JXAaDOnwpB1ZGO",
+                             "rp_token_created_at":"2019-08-18 13:18:48",
+                              "default_billing":null,
+                               "default_shipping":null,
+                                "taxvat":null,
+                                 "confirmation":null,
+                                  "gender":null,
+                                   "failures_num":"0",
+                                    "first_failure":null,
+                                     "lock_expires":null}',
+            true
+        );
         $writer->write($this->filePath, [
             ['action' => 'created', 'customer_id' => 11, 'data' => $customer],
             ['action' => 'deleted', 'customer_id' => 54],
--- Violeta/CronModule/Test/Unit/JsonWriterTest.php
+++ Violeta/CronModule/Test/Unit/JsonWriterTest.php
@@ -62,34 +62,7 @@ class JsonWriterTest extends TestCase
         ]);
         $contents = $this->readJson($this->filePath);
         $this->assertEquals(
-            '[{"action":"added","customer_id":11,
-            "data":{"entity_id":"11",
-            "website_id":"1",
-            "email":"whatever1@gmail.com",
-            "group_id":"1","increment_id":null,
-            "store_id":"2",
-            "created_at":"2019-08-18 13:18:47",
-            "updated_at":"2019-08-18 13:18:48",
-            "is_active":"1",
-            "disable_auto_group_change":"0",
-            "created_in":"Second store view",
-            "prefix":null,
-            "firstname":"Petras",
-            "middlename":null,
-            "lastname":"Pirmasis",
-            "suffix":null,
-            "dob":null,
-            "password_hash":"2d4788574f6b918a4d1fd9435f25621b0cae38559e85fdeb45856b3c9df7d6cb:kg2Jzguul7XIeXDI085fNLTK9B1MvqTB:1",
-            "rp_token":"mz5qlwATAuQ7id3LD5JXAaDOnwpB1ZGO",
-            "rp_token_created_at":"2019-08-18 13:18:48",
-            "default_billing":null,
-            "default_shipping":null,
-            "taxvat":null,
-            "confirmation":null,
-            "gender":null,
-            "failures_num":"0",
-            "first_failure":null,
-            "lock_expires":null}}]',
+            '[{"action":"added","customer_id":11,"data":{"entity_id":"11","website_id":"1","email":"whatever1@gmail.com","group_id":"1","increment_id":null,"store_id":"2","created_at":"2019-08-18 13:18:47","updated_at":"2019-08-18 13:18:48","is_active":"1","disable_auto_group_change":"0","created_in":"Second store view","prefix":null,"firstname":"Petras","middlename":null,"lastname":"Pirmasis","suffix":null,"dob":null,"password_hash":"2d4788574f6b918a4d1fd9435f25621b0cae38559e85fdeb45856b3c9df7d6cb:kg2Jzguul7XIeXDI085fNLTK9B1MvqTB:1","rp_token":"mz5qlwATAuQ7id3LD5JXAaDOnwpB1ZGO","rp_token_created_at":"2019-08-18 13:18:48","default_billing":null,"default_shipping":null,"taxvat":null,"confirmation":null,"gender":null,"failures_num":"0","first_failure":null,"lock_expires":null}}]',
             $contents
         );
     }
